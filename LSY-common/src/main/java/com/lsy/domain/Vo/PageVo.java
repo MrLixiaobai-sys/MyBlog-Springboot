4915ed1a7132ec65f94cdb8f881a918cacfa24a5
pingYun01/shuYinLi <1461760901@qq.com>
2025-01-15 14:40:39 +0800

    add:mybatis-plus分页查询配置类 add:分页封装Vo类

---
+++ LSY-common/src/main/java/com/lsy/domain/Vo/PageVo.java
@@ -0,0 +1,19 @@
+package com.lsy.domain.Vo;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageVo {
+
+   private List rows;
+
+   //总条数
+   private Long total;
+}
