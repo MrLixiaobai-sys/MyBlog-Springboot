f569bbcac7cd80f5e6c5fc08546a94be8623eb21
pingYun01/shuYinLi <1461760901@qq.com>
2025-01-16 15:57:19 +0800

    实现登录功能： common模块: 业务逻辑： 通过security依赖进行账号校验 自定义UserDetailServiceImpl模块进行查询数据库进行用户校验 security调用密码校验(加密)与数据库(加密)进行校验 通过jwt生成token 将认证信息存入到redis(24小时时效),用于下次方便携带token直接从redis缓存查询用户信息进行校验 add:定义响应封装类：token，UserInfo封装类等
    frontend模块:
    add:BlogLoginController实现登录功能

---
+++ LSY-common/src/main/java/com/lsy/domain/Vo/BlogUserLoginVo.java
@@ -0,0 +1,13 @@
+package com.lsy.domain.Vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogUserLoginVo {
+    private String token;
+    private UserInfoVo userInfo;
+}
