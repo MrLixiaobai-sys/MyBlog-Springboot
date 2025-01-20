df849f052d13966869b866c4e9b3214e2e9b6762
pingYun01/shuYinLi <1461760901@qq.com>
2025-01-20 19:42:11 +0800

    fix:更改查询友链评论URL

--- LSY-frontend/src/main/java/com/lsy/Controller/LinkController.java
+++ LSY-frontend/src/main/java/com/lsy/Controller/LinkController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Link")
+@RequestMapping("/link")
 public class LinkController {
 
     @Autowired
