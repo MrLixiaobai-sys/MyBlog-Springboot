dcb7881b69bb3543887fffb6d6bc6f6aab23c58a
pingYun01/shuYinLi <1461760901@qq.com>
2025-01-21 15:33:06 +0800

    优化获取最热门文章 fix:获取最热门10条文章 从原先的直接从数据库里面查询数据,按viewCount排序,由于数据库不是实时的,且性能较低,换成直接从redis中读取viewCount最高的10个文章id,进行查询,保证了热门文章的实时性

--- LSY-common/src/main/java/com/lsy/service/Imple/ArticleServiceImple.java
+++ LSY-common/src/main/java/com/lsy/service/Imple/ArticleServiceImple.java
@@ -20,11 +20,14 @@ import com.lsy.service.CategoryService;
 import com.lsy.utils.BeanCopyUtils;
 import com.lsy.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ArticleServiceImple extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
@@ -36,24 +39,32 @@ public class ArticleServiceImple extends ServiceImpl<ArticleMapper, Article> imp
     private ArticleService articleService;
     @Autowired
     private RedisCache redisCache;
+
+    @Autowired
+    private ArticleMapper articleMapper;
     //    获取最多浏览量的10条文章
     @Override
     public ResponseResult getMostViewsArticle() {
 
-//        通过mybatis-plus的LambdaQueryWrapper查询出浏览量最多的10条文章
-        LambdaQueryWrapper<Article> wrapper = new QueryWrapper<Article>().lambda();
-//        查询出正式发布的文章(状态0为正式发布,1为草稿中)
-//        1.增加查询条件:状态为0(发布状态)
-        wrapper.eq(Article::getStatus, ArticleStatus.ARTICLE_STATUS_NORMAL);
-//        2.按照浏览量降序
-        wrapper.orderByDesc(Article::getViewCount);
-//        3.分页查询,并获取10条数据
-//        定义分页参数
-        Page<Article> page = new Page<>(1, 10);
-//        分页查询并将数据封装到page对象中
-        page(page, wrapper);
-//        获取10条查询结果
-        List<Article> records = page.getRecords();
+////        通过mybatis-plus的LambdaQueryWrapper查询出浏览量最多的10条文章
+//        LambdaQueryWrapper<Article> wrapper = new QueryWrapper<Article>().lambda();
+////        查询出正式发布的文章(状态0为正式发布,1为草稿中)
+////        1.增加查询条件:状态为0(发布状态)
+//        wrapper.eq(Article::getStatus, ArticleStatus.ARTICLE_STATUS_NORMAL);
+////        2.按照浏览量降序
+//        wrapper.orderByDesc(Article::getViewCount);
+////        3.分页查询,并获取10条数据
+////        定义分页参数
+//        Page<Article> page = new Page<>(1, 10);
+////        分页查询并将数据封装到page对象中
+//        page(page, wrapper);
+////        获取10条查询结果
+//        List<Article> records = page.getRecords();
+
+        List<String> ids = getTop10FromHash();
+        List<Article> records = articleMapper.selectBatchIds(ids);
+        // 按照 viewCount 降序排序
+        records.sort((a, b) -> Long.compare(b.getViewCount(), a.getViewCount()));
 
         List<MostViewArticleVo> vs = BeanCopyUtils.copyBeanList(records, MostViewArticleVo.class);
         return ResponseResult.okResult(vs);
@@ -133,5 +144,20 @@ public class ArticleServiceImple extends ServiceImpl<ArticleMapper, Article> imp
     public ResponseResult updateViewCount(Long id) {
         redisCache.increaseCacheViewCountValue(ArticleStatus.REDIS_ARTICLE_VIEWCOUNT, String.valueOf(id), ArticleStatus.INCREATE_ARTICLE_VIEW_COUNT);
         return ResponseResult.okResult();
+    }
+
+    //得到redis中浏览量排序前10的对应的key
+    public List<String> getTop10FromHash() {
+
+        Map<String, Integer> viewCountMap = redisCache.getCacheMap(ArticleStatus.REDIS_ARTICLE_VIEWCOUNT);
+        // 对键值对按值（viewCounts）降序排序，并取前 10
+        List<String> top10Keys = viewCountMap.entrySet().stream()
+                .sorted((entry1, entry2) -> Integer.compare(
+                        Integer.valueOf(entry2.getValue().toString()), // 按值降序
+                        Integer.valueOf(entry1.getValue().toString())))
+                .limit(10)
+                .map(entry -> entry.getKey().toString()) // 提取键（key）
+                .collect(Collectors.toList());
+        return top10Keys;
     }
 }
